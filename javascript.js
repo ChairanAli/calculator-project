0130721ce8690dc44d1d9f400c6776368deda5e7
ChairanAli <90807825+ChairanAli@users.noreply.github.com>
2021-10-10 15:34:12 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,104 @@
+const numbers = document.querySelectorAll(".number")
+
+// memanggil number agar dapat ditampilkan pada layar saat di klik//
+numbers.forEach((number) => {
+    number.addEventListener("click", (event) => {
+        inputNumber(event.target.value)
+        updateScreen(currentNumber)
+    })
+})
+
+const calculatorScreen = document.querySelector('.calculator-screen')
+
+const updateScreen = (number) => {
+    calculatorScreen.value = number
+}
+
+const inputNumber = (number) => {
+    if (currentNumber === '0') {
+        currentNumber = number
+    } else {
+        currentNumber += number
+    }
+}
+
+let prevNumber = ''
+let calculationOperator = ''
+let currentNumber= '0'
+
+const operators = document.querySelectorAll(".operator")
+
+operators.forEach((operator) => {
+operator.addEventListener("click", (event) => {
+        inputOperator(event.target.value)
+    })
+})
+
+const inputOperator = (operator) => {
+    if (calculationOperator === '') {
+        prevNumber = currentNumber
+    }
+    calculationOperator = operator
+    currentNumber = '0'
+}
+
+const equalSign = document.querySelector('.equal-sign')
+
+equalSign.addEventListener('click', () => {
+    calculate()
+    updateScreen(currentNumber)
+})
+
+const calculate = () => {
+    let result = ''
+    switch(calculationOperator) {
+        case "+":
+            result = parseFloat (prevNumber) + parseFloat (currentNumber)
+            break
+        case "-":
+            result = parseFloat (prevNumber) - parseFloat (currentNumber)
+            break
+        case "*":
+            result = parseFloat (prevNumber) * parseFloat (currentNumber)
+            break
+        case "/":
+            result = parseFloat (prevNumber) / parseFloat (currentNumber)
+            break
+        case "%":
+            result = parseFloat (prevNumber) % parseFloat (currentNumber)
+            break
+        default:
+            return
+    }
+    currentNumber = result
+    calculationOperator = ''
+}
+//mengaktifkan tombol AC untuk clear-all//
+const clearAll = () => {
+    prevNumber = ''
+    calculationOperator = ''
+    currentNumber = '0'
+}
+
+const clearBtn = document.querySelector('.all-clear')
+
+clearBtn.addEventListener('click', () => {
+    clearAll()
+    updateScreen(currentNumber)
+})
+
+//menambahkan tanda titik untuk fungsi desimal//
+const decimal = document.querySelector('.decimal')
+
+decimal.addEventListener('click', (event) => {
+    inputDecimal(event.target.value)
+    updateScreen(currentNumber)
+})
+
+inputDecimal = (dot) => {
+    if(currentNumber.includes('.')){
+        return
+    }
+        currentNumber += dot
+ }
+ 
